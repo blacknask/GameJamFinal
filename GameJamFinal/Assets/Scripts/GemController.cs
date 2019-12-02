d9e25f76c079fe5c57533c4b71e23c10ef27932a
momoaden <denisbob21@gmail.com>
2019-12-02 17:33:53 -0300

    Testes de mecânica

---
+++ GameJamFinal/Assets/Scripts/GemController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemController : MonoBehaviour
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 1, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            GetComponent<AudioSource>().Play();
+            GetComponent<Renderer>().enabled = false;
+            GetComponent<CapsuleCollider>().enabled = false;
+        }
+    }
+}
